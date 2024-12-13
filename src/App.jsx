c3e6c9bef286f81cdd4faa2f188948b0739fd62c
rohan <gargrohan138@gmail.com>
2024-12-13 13:04:23 +0530

    adding tictactoe game project

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     "https://github.com/Sandstorm831/Learning-react/tree/main/shopping_cart",
     "https://github.com/Sandstorm831/Learning-react/tree/main/coalition_project",
     "https://github.com/Sandstorm831/Learning-react/tree/main/businessCards",
+    "https://github.com/Sandstorm831/Learning-react/tree/main/Tic_Tac_Toe",
   ];
   const live_demo_links = [
     "/todoapp",
@@ -19,6 +20,7 @@ function App() {
     "/shoppingcart",
     "/coalitionproject",
     "/businesscardapp",
+    "/tictactoe",
   ];
   const sources = [
     "/static/images/todo_fullstack1.png",
@@ -28,6 +30,7 @@ function App() {
     "static/images/shoppingCart.png",
     "/static/images/coalitionProject.png",
     "/static/images/skullCard.png",
+    "/static/images/tictactoe.png",
   ];
   const titles = [
     "To-Do Applicatoin",
@@ -36,7 +39,8 @@ function App() {
     "Custom React Components",
     "Shopping Web Application",
     "Coalition CSS Project",
-    "Business Card Application"
+    "Business Card Application",
+    "Tic-Tac-Toe Game",
   ];
   const descriptions = [
     "This is a simple To-Do app where users can create tasks, mark them as done, and delete them.",
@@ -46,6 +50,7 @@ function App() {
     "This a minimal shopping cart, here you can find the products, include them in your cart. All the products you have included in cart will be shown in the Cart page and the total price will also be displayed there. This project uses Recoil library for state management instead of Context API.",
     "This is a CSS-intensive project that I completed as an assignment for a company called Coalition. The task was to precisely replicate a given webpage, and this is the final result. I used ChartJS for the graph and TailwindCSS for styling. Functionality was not included, as it was not required for the assignment.",
     "This is my first project in the direction of learning serious CSS and styling skills. In this project, each card have an embedded animation. On hovering on any of card, the title of card moves up, and a description about the card holder and it's social media handles appears in smooth transition, and disappears smoothly on hovering out.",
+    "Made a simple tic-tac-toe game while learning React, this was my first project in React and made it by following the instructions from react.dev website's learning section. I made two versions, first one was my original, but was very verbose, second was of the website's which was short and easy to understand.",
   ];
   let counter = 0;
   const divArray = [];
